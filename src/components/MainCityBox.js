763dee9e6c8aa632772fe0c0d422b7113bf6a73f
GianlucaDore <gianluca.dore@yahoo.it>
2023-05-15 17:10:23 +0200

    v1.0 even more responsive + diff bckgs for main city

--- src/components/MainCityBox.js
+++ src/components/MainCityBox.js
@@ -9,7 +9,7 @@ export const MainCityBox = () =>
 
     return (
         <div id="main_city_box">
-            <div id="main_city_data">
+            <div id={"torino_" + data.weather_description.replaceAll(' ', '_')}>
                 <h2 className='main_city_details'>{ data.name }</h2>
                 <p className='main_city_details'>{ getWeekday(data.time.getDay()) } { data.time.getDate() }, {getMonth(data.time.getMonth())}</p>
                 <p className='main_city_details'>{ data.weather }: { data.weather_description }</p>
